e48f170beb0bf780f6f6b3a0cf430a1a45e72e19
Karl Horky <karl.horky@gmail.com>
2016-01-03 18:07:48 +0100

    Add shallow DOM test

--- test/index.test.js
+++ test/index.test.js
@@ -2,16 +2,21 @@
 
 import React from 'react';
 import MalarkeyComponent from '../index';
-import {describeWithDOM, mount, spyLifecycle} from 'enzyme';
+import {shallow, describeWithDOM, mount, spyLifecycle} from 'enzyme';
 
 import {expect} from 'chai';
 
 describe('MalarkeyComponent', () => {
-  describeWithDOM('MalarkeyComponent DOM', () => {
-    it('should mount', () => {
-      spyLifecycle(MalarkeyComponent);
-      mount(<MalarkeyComponent />);
-      expect(MalarkeyComponent.prototype.componentDidMount.calledOnce).to.be.true;
-    });
+  it('should render a <div />', () => {
+    const wrapper = shallow(<MalarkeyComponent />);
+    expect(wrapper.find('div')).to.have.length(1);
+  });
+});
+
+describeWithDOM('MalarkeyComponent DOM', () => {
+  it('should mount', () => {
+    spyLifecycle(MalarkeyComponent);
+    mount(<MalarkeyComponent />);
+    expect(MalarkeyComponent.prototype.componentDidMount.calledOnce).to.be.true;
   });
 });
