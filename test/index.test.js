532b2fa5c357a5c2c8747cb902f30180d419b57d
Karl Horky <karl.horky@gmail.com>
2016-01-03 18:52:41 +0100

    Add BABEL_ENV to npm test script

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ import {shallow, describeWithDOM, mount, spyLifecycle} from 'enzyme';
 import {expect} from 'chai';
 
 describe('MalarkeyComponent', () => {
-  it('should render a <div />', () => {
+  it('should render one <div />', () => {
     const wrapper = shallow(<MalarkeyComponent />);
     expect(wrapper.find('div')).to.have.length(1);
   });
