73edcc2049951cd03724d2b6caab0ec69909ec02
acidicX <webmaster@carstenm.de>
2016-01-25 17:09:44 +0100

    changed to new react class syntax, added possibility to render static markup, component now uses a span element, updated dependencies, split dependencies

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import malarkey from 'malarkey';
 
-const MalarkeyComponent = React.createClass({
-  getDefaultProps () {
-    return {
-      messages: [],
-      options: {
-        loop: true
-      },
-      sequence (m, messages) {
-        messages.forEach(message => {
-          m.type(message).pause().delete();
-        });
-      }
-    };
-  },
+export default class Malarkey extends React.Component {
+  constructor (props) {
+    super(props);
+  }
 
   componentDidMount () {
     const {messages, options, sequence} = this.props;
     const m = malarkey(this.node, options);
     sequence(m, messages);
-  },
+  }
 
   render () {
-    return <div ref={node => this.node = node} />;
+    const {messages, staticRender} = this.props;
+    if (staticRender) {
+      // return a random message on static rendering
+      return <span>{ messages[Math.floor(Math.random() * messages.length)] }</span>;
+    }
+    return <span ref={node => this.node = node} />;
   }
-});
+}
 
-export default MalarkeyComponent;
+Malarkey.displayName = 'Malarkey';
+Malarkey.defaultProps = {
+  messages: [],
+  options: {
+    loop: true
+  },
+  sequence (m, messages) {
+    messages.forEach(message => {
+      m.type(message).pause().delete();
+    });
+  },
+  staticRender: false
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,33 +1,39 @@
 'use strict';
 
 import React from 'react';
-import MalarkeyComponent from '../index';
+import Malarkey from '../index';
 import {shallow, describeWithDOM, mount, spyLifecycle} from 'enzyme';
 
 import {expect} from 'chai';
 
 describe('MalarkeyComponent', () => {
-  it('should render one <div />', () => {
-    const wrapper = shallow(<MalarkeyComponent />);
-    expect(wrapper.find('div')).to.have.length(1);
+  it('should render one <span />', () => {
+    const wrapper = shallow(<Malarkey />);
+    expect(wrapper.find('span')).to.have.length(1);
   });
 });
 
 describeWithDOM('MalarkeyComponent DOM', () => {
   it('should mount', () => {
-    spyLifecycle(MalarkeyComponent);
-    mount(<MalarkeyComponent />);
-    expect(MalarkeyComponent.prototype.componentDidMount.calledOnce).to.be.true;
+    spyLifecycle(Malarkey);
+    mount(<Malarkey />);
+    expect(Malarkey.prototype.componentDidMount.calledOnce).to.be.true;
   });
 
   it('should render text from props', () => {
     const messages = ['a', 'b'];
-    const wrapper = mount(<MalarkeyComponent messages={messages} />);
+    const wrapper = mount(<Malarkey messages={messages} />);
     const promise = new Promise(resolve => {
       setTimeout(() => { return resolve(wrapper.text()); }, 60);
     });
     return promise.then(message => {
       expect(message).to.equal(messages[0]);
     });
+  });
+
+  it('should render static markup', () => {
+    const messages = ['a'];
+    const wrapper = mount(<Malarkey messages={messages} staticRender={true} />);
+    expect(wrapper.text()).to.equal(messages[0]);
   });
 });
